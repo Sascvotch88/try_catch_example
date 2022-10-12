22e4fc8a9626dff3a835dc26db6de4e5a3ff5bff
Sascvotch <sascvotch@yandex.ru>
2022-10-12 21:20:11 +0300

    пример перехвата и обработки исключения в блоке try catch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader("D:/text.txt"));
+            String str = buffer.readLine();
+            System.out.println(str);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            System.out.println("finally");
+        }
+
     }
 }
